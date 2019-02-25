8b7d731172791b81dc0d92e0b3f10e7f25f2061b
onuriltan <onur.iltan@gmail.com>
2019-02-25 23:41:26 +0300

    add bg to login

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
     <Header/>
-    <div class="container">
-      <router-view/>
-    </div>
+    <router-view/>
   </div>
 </template>
 
--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="login-container">
-    <form class="login-container__form">
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+  <div class="login">
+    <div class="login-container container">
+      <form class="login-container__form">
+        <h2 class="text-center pb-3">Login</h2>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                 placeholder="Enter email">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-
-  }
+export default {}
 </script>
 
 <style scoped lang="scss">
- @import "../styles/components/Login";
+  @import "../styles/components/Login";
 </style>
